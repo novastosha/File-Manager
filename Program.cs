7f0a99c207b18d1b1e1e02ab1c3712f9aee0e6d4
novastosha <galliigotu@gmail.com>
2021-06-10 10:48:20 +0100

    Added File filters added list Files added attribute manager, added create file, added create dir(s)

--- Program.cs
+++ Program.cs
@@ -5,10 +5,14 @@ namespace FileManagerC_ {
     class Program
     {
         static void Main(string[] args) {
+
+            
             foreach (FileAPI.File file in new FileAPI.File("/mnt/0E9E8E9B9E8E7B4B/lol").listFiles())
             {
                 Console.WriteLine(file.getName());               
             }
+
+            new FileAPI.
         }
     }
 }
@@ -33,6 +37,50 @@ namespace FileAPI {
         public File getFile() {
             return file;
         }
+
+        public string coombineToEnd(int end) {
+            int index = 0;
+            string fin = "";
+            foreach (string s in seperated) {
+                if(index <= end){
+                    fin = fin+"/"+s;
+                }
+                index++;
+            }
+            return fin;
+        }
+
+        //TODO: From Start
+    }
+
+    class FileSecurityManager {
+
+        private File file;
+        private FileAttributes attributes;
+        public FileSecurityManager(File file) {
+            this.file = file;
+           // this.attributes = System.IO.File.GetAttributes(file.getPath());
+        }
+    }
+
+    abstract class FileFilter {
+
+        public abstract bool pass(File file);
+
+    }
+
+    class FileExtensionFilter : FileFilter {
+        
+        private string extension;
+        
+        public FileExtensionFilter(string extension) {
+            this.extension = extension;
+        }
+
+        public override bool pass(File file) {
+            if(file.getFileExtension().Equals(extension)) return true;
+            else return false;
+        }
     }
     class File {
 
@@ -40,7 +88,10 @@ namespace FileAPI {
         private PathSeparator pathSeparator;
         private string name;
         private List<File> files;
+        private FileSecurityManager securityManager;
+
         public File(string path) {
+            this.securityManager = new FileSecurityManager(this);
             this.path = path.Replace(@"\","/",true,null);
             this.pathSeparator = new PathSeparator(this);
             this.name = pathSeparator.getSeperated()[pathSeparator.getSeperated().Length-1];
@@ -61,6 +112,46 @@ namespace FileAPI {
             }
         }
 
+        public bool createFile() {
+            
+            try
+            {
+                System.IO.File.Create(path);
+                return true;    
+            }
+            catch (System.Exception e)
+            {
+                
+                throw new System.IO.IOException(e.Message);
+            }
+        }
+
+        public bool mkdir() {
+            try {
+                System.IO.Directory.CreateDirectory(path);
+                return true;
+            }
+            catch(System.Exception) {
+                return false;
+            }
+        }
+
+        public bool mkdirs() {
+            try {
+                int index = 0;
+                foreach(string p in pathSeparator.getSeperated()) {
+                    string com = pathSeparator.coombineToEnd(index);
+                    if(!System.IO.Directory.Exists(com)) {
+                        System.IO.Directory.CreateDirectory(com);
+                    }
+                    index++;
+                }
+                return true;
+            }catch(System.Exception e) {
+                return false;
+            }
+        }
+        
         public string getPath() {
             return path;
         }
@@ -126,7 +217,6 @@ namespace FileAPI {
         }
 
         public List<File> listFiles() {
-            
             return files;
         }
 
